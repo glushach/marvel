8279c09ef63faf89e95248f4db550a6fa65016eb
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-07-14 09:51:17 +0300

    feat: render chars in main section

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,8 @@
 class MarvelService {
   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-  _apiKey = 'apikey=7d132ada261db967de78c1cb7f7b5de9';
+  // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
+  _apiKey = 'apikey=c5d6fc8b83116d92ed468ce36bac6c62';
+
   getResource = async (url) => {
     let res = await fetch(url);
 
@@ -12,26 +14,24 @@ class MarvelService {
   }
 
   getAllCharacters = async () => {
-    const res = await this.getResource(
-      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
-    );
-    return res.data.results.map(this._transformCharacter); // custom call back function
+    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    return res.data.results.map(this._transformCharacter);
   }
 
   getCharacter = async (id) => {
-    const res = await this.getResource(
-      `${this._apiBase}characters/${id}?${this._apiKey}`
-    );
+    const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
     return this._transformCharacter(res.data.results[0]);
   }
 
   _transformCharacter = (char) => {
     return {
+      id: char.id,
       name: char.name,
-      description: char.description,
+      description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
       thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
       homepage: char.urls[0].url,
-      wiki: char.urls[1].url
+      wiki: char.urls[1].url,
+      comics: char.comics.items
     }
   }
 }
